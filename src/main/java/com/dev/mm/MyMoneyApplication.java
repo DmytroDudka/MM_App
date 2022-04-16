7216cb6b8a43477999a5296974f52e9f0603759c
Dmitriy <dudka.dmytro@icloud.com>
2022-04-17 00:40:41 +0300

    Initial commit

---
+++ src/main/java/com/dev/mm/MyMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.dev.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyApplication.class, args);
+	}
+
+}
